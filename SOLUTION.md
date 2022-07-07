098ea2bff40edef468cf9cd2d3813544407d1204
Christopher Kellner <=>
2022-07-07 16:13:41 +0200

    [add] solution file for notes [fix#task1] provided AbstractSearchFacadeService using concrete implementation

---
+++ SOLUTION.md
@@ -0,0 +1,8 @@
+# Notizen zu den Lösungsschritten
+
+## Aufgabe 1
+- AbstractSearchFacadeService kann nicht injected werden (aus dem Dashboard Modul heraus) - siehe Browser
+- Konkrete Implementierung SearchFacadeService wird provided, nicht aber die BaseClass
+- switch that and go
+
+## Aufgabe 2
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { PractitionerSearchService } from './services/practitioner-search.service';
 import { SearchFacadeService } from './services/search-facade.service';
 import { PatientSearchService } from './services/patient-search.service';
+import { AbstractSearchFacadeService } from './services/abstract-search-facade.service';
 
 @NgModule({
-  providers: [PractitionerSearchService, PatientSearchService, SearchFacadeService],
+  providers: [
+    PractitionerSearchService,
+    PatientSearchService,
+    
+    // kann weg - die konkrete Implementierung wird nirgends injected
+    // SearchFacadeService,
+
+    // muss dazu - um das injecten von AbstractSearchFacadeService im Dashboard zu ermöglichen
+    { provide: AbstractSearchFacadeService, useClass: SearchFacadeService }
+  ],
   bootstrap: [],
 })
-export class SearchModule {}
+export class SearchModule { }
